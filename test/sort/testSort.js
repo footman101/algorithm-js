01d45b9a748cc3b0471c0df4109c5480de8c353b
footman101 <houyuxin82@gmail.com>
2015-10-11 17:02:29 +0800

    quick sort

---
+++ test/sort/testSort.js
@@ -0,0 +1,14 @@
+/*
+ * @file 排序测试
+ * @author footman101 (houyuxin82@gmail.com)
+ * @date Sun Oct 11 2015
+ */
+
+var random = require('../../lib/random/random');
+var quickSort = require('../../lib/sort/quickSort');
+
+// 快速排序
+var array = random.array(10);
+console.log(array);
+quickSort.sort(array);
+console.log(array);
