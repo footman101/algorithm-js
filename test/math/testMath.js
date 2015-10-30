4800fbfd772b629a0fbe8c108676384ecd75f98e
footman101 <houyuxin82@gmail.com>
2015-10-30 20:53:19 +0800

    add math gcd

---
+++ test/math/testMath.js
@@ -0,0 +1,12 @@
+/*
+ * @file Math工具类测试
+ * @author footman101 (houyuxin82@gmail.com)
+ * @date Fri Oct 30 2015
+ */
+
+var mathUtil = require('../../lib/math/util');
+
+var p = 100;
+var q = 50;
+console.log(mathUtil.gcd(p, q));
+
